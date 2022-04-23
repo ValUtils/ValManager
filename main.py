79b9173510607dd89763980c946e87772d141f40
PWall <34860495+PWall2222@users.noreply.github.com>
2022-04-24 01:34:25 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,106 @@
+from pick import pick as pickFunc
+from getpass import getpass 
+from sys import argv
+from riot import getHeaders
+import requests
+import json
+import zlib
+import base64
+
+def pick(options):
+    return pickFunc(options)[0]
+
+def jsonRead(file):
+    f = open(file, "r")
+    rawData = f.read()
+    data = json.loads(rawData)
+    f.close()
+    return data
+
+def decode( b64string ):
+    decoded_data = base64.b64decode( b64string )
+    return zlib.decompress( decoded_data , -15)
+
+
+def encode( string_val ):
+    zlibbed_str = zlib.compress( string_val )
+    compressed_string = zlibbed_str[2:-4]
+    return base64.b64encode( compressed_string )
+
+def toData( b64string ):
+    inflated_data = decode(b64string)
+    return json.loads(inflated_data)
+
+def toMagic( data ):
+    stringify = json.dumps(data).encode("utf-8")
+    return encode(stringify).decode("utf-8")
+
+def getPreference():
+    apiURL = 'https://playerpreferences.riotgames.com/playerPref/v3/getPreference/Ares.PlayerSettings'
+    rawData = requests.get(apiURL, headers=headers)
+    jsonData = json.loads(rawData.text)
+    data = toData(jsonData["data"])
+    return data
+
+def setPreference(data):
+    rawData = {
+        "type": "Ares.PlayerSettings",
+        "data": toMagic(data)
+    }
+    apiURL = 'https://playerpreferences.riotgames.com/playerPref/v3/savePreference'
+    req = requests.put(apiURL, headers=headers, json=rawData)
+    return req
+
+def dumpToFile(cfg):
+    f = open(cfg, "w")
+    f.write(json.dumps(getPreference(),indent=4))
+    f.close()
+
+def importFromFile(cfg):
+    data = jsonRead(cfg)
+    req = setPreference(data)
+    print(f'Status code: {req.status_code}')
+
+def getUsers():
+    users = jsonRead("users.json")
+    return list(users.keys())
+
+def getPass(user):
+    users = jsonRead("users.json")
+    if user in users:
+        return users[user]
+    return ""
+
+def main():
+    action, cfg, user = menu()
+    passwd = getPass(user)
+    
+    if (user == "None" or passwd == ""):
+        user = input("User: ")
+        passwd = getpass("Password: ")
+
+    global headers
+    headers = getHeaders(user, passwd)
+
+    if (action == "dump"):
+        dumpToFile(cfg)
+    elif (action == "import"):
+        dumpToFile(f'{user}.bck.json')
+        importFromFile(cfg)
+    elif (action == "restore"):
+        importFromFile(f'{user}.bck.json')
+
+def menu():
+    if (len(argv) > 2):
+        return [argv[1],argv[2],argv[3]]
+    action = pick(["dump", "import", "restore"])
+    users = getUsers()
+    users.append("None")
+    user = pick(users)
+    if (len(argv) > 1):
+        return [action, argv[1], user]
+    cfg = input("Config file: ")
+    return [action, cfg, user]
+
+if __name__ == "__main__":
+    main()
---
+++ riot.py
@@ -0,0 +1,77 @@
+import ssl
+import requests
+from collections import OrderedDict
+import re
+import socket
+from requests.adapters import HTTPAdapter
+from urllib3 import PoolManager
+
+client_platform = 'ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9'
+
+def authenticate(username, password):
+    class SSLAdapter(HTTPAdapter):
+        def init_poolmanager(self, connections, maxsize, block=False):
+            self.poolmanager = PoolManager(num_pools=connections,
+                                        maxsize=maxsize,
+                                        block=block,
+                                        ssl_version=ssl.PROTOCOL_TLSv1_2)
+
+    headers = OrderedDict({
+        'User-Agent': 'RiotClient/48.0.0.4342439.4342439 rso-auth (Windows;10;;Professional, x64)'
+    })
+
+    session = requests.session()
+    session.mount('https://auth.riotgames.com/api/v1/authorization', SSLAdapter())
+    session.headers = headers
+
+    data = {
+        'client_id': 'play-valorant-web-prod',
+        'nonce': '1',
+        'redirect_uri': 'https://playvalorant.com/opt_in',
+        'response_type': 'token id_token',
+    }
+    r = session.post(f'https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
+
+    data = {
+        'type': 'auth',
+        'username': username,
+        'password': password
+    }
+    r = session.put(f'https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
+    pattern = re.compile('access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
+    data = pattern.findall(r.json()['response']['parameters']['uri'])[0]
+    access_token = data[0]
+
+    headers = {
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Host': "entitlements.auth.riotgames.com",
+        'User-Agent': 'RiotClient/43.0.1.4195386.4190634 rso-auth (Windows;10;;Professional, x64)',
+        'Authorization': f'Bearer {access_token}',
+    }
+    r = session.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers, json={})
+    entitlements_token = r.json()['entitlements_token']
+
+    headers = {
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Host': "auth.riotgames.com",
+        'User-Agent': 'RiotClient/43.0.1.4195386.4190634 rso-auth (Windows;10;;Professional, x64)',
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    r = session.post('https://auth.riotgames.com/userinfo', headers=headers, json={})
+    user_id = r.json()['sub']
+    headers['X-Riot-Entitlements-JWT'] = entitlements_token
+    del headers['Host']
+    session.close()
+    return headers
+
+def getVersion():
+    data = requests.get('https://valorant-api.com/v1/version')
+    data = data.json()['data']
+    return data["riotClientVersion"]
+
+def getHeaders(username, password):
+    headers = authenticate(username, password)
+    headers['X-Riot-ClientPlatform'] = client_platform
+    headers['X-Riot-ClientVersion'] = getVersion()
+    return headers
