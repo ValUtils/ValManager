d42f330686c144cc01afa17bb14d0144a206ec2a
PWall <34860495+PWall2222@users.noreply.github.com>
2022-06-04 13:23:20 +0200

    refactor: move parsing to separate file

---
+++ parsing.py
@@ -0,0 +1,20 @@
+import json
+import zlib
+import base64
+
+def decode( b64string ):
+    decoded_data = base64.b64decode( b64string )
+    return zlib.decompress( decoded_data , -15)
+
+def encode( string_val ):
+    zlibbed_str = zlib.compress( string_val )
+    compressed_string = zlibbed_str[2:-4]
+    return base64.b64encode( compressed_string )
+
+def toData( b64string ):
+    inflated_data = decode(b64string)
+    return json.loads(inflated_data)
+
+def toMagic( data ):
+    stringify = json.dumps(data).encode("utf-8")
+    return encode(stringify).decode("utf-8")
