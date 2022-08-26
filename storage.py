de8638d340451b201379681cf887db37debb280a
PWall <34860495+PWall2222@users.noreply.github.com>
2022-08-26 23:04:17 +0200

    style: convert storage.py to 4 spaces

--- storage.py
+++ storage.py
@@ -4,73 +4,73 @@ import platform
 import json
 
 def getFilePath(file):
-	return joinPath(settingsPath, file)
+    return joinPath(settingsPath, file)
 
 def saveToDrive(data, file):
-	f = open(getFilePath(file), "w")
-	f.write(data)
-	f.close()
+    f = open(getFilePath(file), "w")
+    f.write(data)
+    f.close()
 
 def readFromDrive(file):
-	f = open(getFilePath(file), "r")
-	data = f.read()
-	f.close()
-	return data
+    f = open(getFilePath(file), "r")
+    data = f.read()
+    f.close()
+    return data
 
 def jsonWrite(data, file):
-	jsonData = json.dumps(data,indent=4)
-	saveToDrive(jsonData, file)
+    jsonData = json.dumps(data,indent=4)
+    saveToDrive(jsonData, file)
 
 def jsonRead(file):
-	rawData = readFromDrive(file)
-	data = json.loads(rawData)
-	return data
+    rawData = readFromDrive(file)
+    data = json.loads(rawData)
+    return data
 
 def configWrite(data,file):
-	jsonWrite(data, joinPath("configs", file))
+    jsonWrite(data, joinPath("configs", file))
 
 def configRead(file):
-	return jsonRead(joinPath("configs", file))
+    return jsonRead(joinPath("configs", file))
 
 def configList():
-	return listDir("configs")
+    return listDir("configs")
 
 def loadWrite(data, file, sub):
-	createPath(joinPath(settingsPath, "loadouts", sub))
-	jsonWrite(data, joinPath("loadouts", sub, file))
+    createPath(joinPath(settingsPath, "loadouts", sub))
+    jsonWrite(data, joinPath("loadouts", sub, file))
 
 def loadRead(file, sub):
-	return jsonRead(joinPath("loadouts", sub, file))
+    return jsonRead(joinPath("loadouts", sub, file))
 
 def loadList(sub):
-	return listDir(joinPath(settingsPath, "loadouts", sub))
+    return listDir(joinPath(settingsPath, "loadouts", sub))
 
 def listDir(dir):
-	directory = getFilePath(dir)
-	createPath(directory)
-	contents = os.listdir(directory)
-	files = []
-	for f in contents:
-		isFile = isfile(joinPath(directory, f))
-		if (isFile):
-			files.append(f)
-	return files
+    directory = getFilePath(dir)
+    createPath(directory)
+    contents = os.listdir(directory)
+    files = []
+    for f in contents:
+        isFile = isfile(joinPath(directory, f))
+        if (isFile):
+            files.append(f)
+    return files
 
 def createPath(path):
-	if(isdir(path)):
-		return
-	os.mkdir(path)
+    if(isdir(path)):
+        return
+    os.mkdir(path)
 
 def setPath():
-	global settingsPath
-	if (platform.system() == "Windows"):
-		appdata = os.getenv('APPDATA')
-		settingsPath = appdata + "\\ValConfig"
-		createPath(settingsPath)
-	if (settingsPath):
-		folders = ["configs", "loadouts"]
-		for f in folders:
-			createPath(getFilePath(f))
+    global settingsPath
+    if (platform.system() == "Windows"):
+        appdata = os.getenv('APPDATA')
+        settingsPath = appdata + "\\ValConfig"
+        createPath(settingsPath)
+    if (settingsPath):
+        folders = ["configs", "loadouts"]
+        for f in folders:
+            createPath(getFilePath(f))
 
 
 setPath()
