bec60461e3572e08041c079eac6b30027b256055
PWall <34860495+PWall2222@users.noreply.github.com>
2022-12-21 23:48:09 +0100

    refactor: move menu functions to separate file

--- ValConfig/main.py
+++ ValConfig/main.py
@@ -1,21 +1,8 @@
-from pick import pick as pickFunc
-from getpass import getpass as inputPass
-from sys import argv
-from ValVault import (
-	init_vault,
-	get_aliases,
-	get_name,
-	get_pass,
-	new_user,
-	User
-)
+from ValVault import init_vault
 
-from .storage import *
-from .loadout import loadout, load_list
-from .config import config, config_list
-
-def pick(options):
-	return pickFunc(options)[0]
+from .loadout import loadout
+from .config import config
+from .tui import menu
 
 def main():
 	init_vault()
@@ -24,50 +11,3 @@ def main():
 		config(action, user, cfg)
 	elif (mode == "loadout"):
 		loadout(action, user, cfg)
-
-def menu():
-	if (len(argv) == 1):
-		return get_options()
-	if (len(argv) == 5):
-		s, mode, action, username, cfg = argv
-		user = User(username, get_pass(username))
-		return [mode, action, user, cfg]
-
-def get_user():
-	users = get_aliases()
-	users.append("New...")
-	choice = pick(users)
-	if (choice != "New..."):
-		return get_name(choice)
-	user = input("User: ")
-	passwd = inputPass("Password: ")
-	new_user(user, passwd)
-	return user
-
-def filter_list(array: list[str], string: str):
-	filteredList = [s for s in array if string not in s]
-	array.clear()
-	array.extend(filteredList)
-
-def choose_file(fileList, dump):
-	if (dump):
-		fileList.append("New...")
-		filter_list(fileList, "backup.json")
-		filter_list(fileList, ".bck.json")
-	choice = pick(fileList)
-	if (choice == "New..."):
-		return input("Filename: ")
-	return choice
-
-def get_options():
-	mode = pick(["config", "loadout"])
-	action = pick(["backup", "dump", "import", "restore"])
-	username = get_user()
-	user = User(username, get_pass(username))
-	if (action in ["restore", "backup"]):
-		return [mode, action, user, ""]
-	if (mode == "config"):
-		cfg = choose_file(config_list(), action == "dump")
-	elif (mode == "loadout"):
-		cfg = choose_file(load_list(username), action == "dump")
-	return [mode, action, user, cfg]
