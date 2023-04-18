2d115639076c6e4ba14bec1cda96258537918a4c
PWall <34860495+PWall2222@users.noreply.github.com>
2023-04-19 00:52:10 +0200

    feat: convert everything to dict before backup

--- ValConfig/backup/transform.py
+++ ValConfig/backup/transform.py
@@ -1,8 +1,14 @@
 import json
 
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 json_keys = ["SavedCrosshairProfileData", "MutedWords"]
+settings_name = {
+    "boolSettings": "EAresBoolSettingName",
+    "floatSettings": "EAresFloatSettingName",
+    "intSettings": "EAresIntSettingName",
+    "stringSettings": "EAresStringSettingName"
+}
 
 
 def get_enum_index(enum: str, settings: list):
@@ -25,19 +31,60 @@ def dump_key(enum: str, strSettings: list):
     setting["value"] = json.dumps(setting["value"])
 
 
+def from_raw_config(config: Dict[str, Any]):
+    loadify(config)
+    magicify(config)
+    return config
+
+
 def to_raw_config(config: Dict[str, Any]):
+    demagicify(config)
+    dumpify(config)
+    return config
+
+
+def dumpify(config):
     if "stringSettings" not in config:
-        return config
+        return
     strSettings = config["stringSettings"]
     for key in json_keys:
         dump_key(key, strSettings)
-    return config
+    return
 
 
-def from_raw_config(config: Dict[str, Any]):
+def loadify(config):
     if "stringSettings" not in config:
-        return config
+        return
     strSettings = config["stringSettings"]
     for key in json_keys:
         load_key(key, strSettings)
-    return config
+    return
+
+
+def magicify(config: Dict[str, Any]):
+    for value_type in settings_name.keys():
+        config[value_type] = dictify(config[value_type])
+
+
+def demagicify(config: Dict[str, Any]):
+    for value_type in settings_name.keys():
+        config[value_type] = undictify(config[value_type], value_type)
+
+
+def dictify(settings: List[Dict[str, Any]]):
+    result = {}
+    for setting in settings:
+        enum = setting["settingEnum"]
+        setting_name = enum.split("::")[1]
+        result[setting_name] = setting["value"]
+    return result
+
+
+def undictify(settings: Dict[str, Any], value_type: str):
+    result = []
+    for setting in settings:
+        result.append({
+            "settingEnum": f"{settings_name[value_type]}::{setting}",
+            "value": settings[setting]
+        })
+    return result
